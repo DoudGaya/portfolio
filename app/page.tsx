68a58f6840114c2f7ba215b45d3bcb25a023adf4
DoudGaya <arbdoule@yahoo.com>
2023-10-27 11:55:16 +0100

    the app

--- app/page.tsx
+++ app/page.tsx
@@ -37,8 +37,8 @@ export default function Home() {
       <div className=" max-w-2xl mx-auto my-2 space-y-2 border-t py-6 items-start w-full">
         <div className=" flex flex-col space-y-2">
           <h1 className=' text-3xl font-bold'>Abdulrahman Dauda</h1>
-          <p className=' font-semibold text-purple-400'>Husband to Amina Shuaibu</p>
-          <p>Super Senior Software Engineer</p>
+          <p>Senior Software Engineer</p>
+          <p className='font-semibold text-primary'>Loving husband to Amena</p>
           <div className=" flex space-x-2 pt-3">
             {
               socials.map((social: Social) => {
@@ -58,14 +58,10 @@ export default function Home() {
             </p>
           </div>
        </div>
-        <div className=" my-3 flex flex-col space-y-3">
+        <div className=" my-3 flex flex-col space-y-3 ">
           <h3 className=' text-2xl font-semibold border-b py-2'>Projects</h3>
-          <div className=" grid grid-cols-2 gap-4">
-
-          </div>
-          {/* <Button>Learn More</Button> */}
-        </div>
             <Projects />
+        </div>
       </div>
    
     </main>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -18,13 +18,14 @@ export const Projects = () => {
       {
         projects.map((project: Project) => {
           return (
-            <div className=" flex space-x-4" key={project.id}>
-              <Image src={project.imgLight} className=' hidden dark:block rounded-md h-[100px] w-[100px] object-cover object-center' alt={project.name} />
-              <Image src={project.imgDark} className=' rounded-md h-20 w-20 block dark:hidden object-cover object-center' alt={project.name} />
-              <div className=" flex flex-col w-full h-full justify-end pb-2" >
+            <div className=" flex  space-x-4" key={project.id}>
+              <div className=" flex-none">
+                <Image src={project.imgLight} className=' hidden dark:block flex-none rounded-md h-[80px] w-[80px] object-cover object-center' alt={project.name} />
+                <Image src={project.imgDark} className=' rounded-md flex-none h-[80px] w-[80px] block dark:hidden object-cover object-center' alt={project.name} />
+              </div>
+              <div className="  flex flex-col w-full h-full justify-end pb-2 " >
                 <Link target='_blank' href={ project.url}><p className=' text-xl hover:text-foreground hover:underline transition-all font-semibold'>{project.name}</p></Link>
                 <p className=' text-sm'> {project.description} </p>
-
               </div>
           </div>
         )
